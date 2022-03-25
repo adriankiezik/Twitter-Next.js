db003e8f872c1811e55276497944b0c4b2edc0c2
adriankiezik <kiezikadrian@gmail.com>
2022-03-25 21:18:29 +0100

    Init commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from "../styles/pages/Home.module.css";
+
+import React from "react";
+
+import type { NextPage } from "next";
+
+const HomePage: NextPage = () => {
+  return <React.Fragment></React.Fragment>;
+};
+
+export default HomePage;
